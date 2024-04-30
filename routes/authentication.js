68da6b76142008992974bfb9471c6805186e49a5
Stina Persson <stpe1901@student.miun.se>
2024-04-30 16:54:54 +0200

    routingen för registrering och login har skapats

--- routes/authentication.js
+++ routes/authentication.js
@@ -4,12 +4,39 @@ const router = express.Router();
 
 //Route för registrering 
 router.post("/register", async (req, res) => {
-    res.json({message: "register called"})
+    try {  
+    const { username, password } = req.body; 
+
+    //validering 
+    if(!username || !password) { //utveckla validering
+        res.status(401).json({message: "Fälten får inte lämnas tomma"})
+    } else {
+        res.status(201).json({message: "Användare skapad!"}); 
+    }
+
+    } catch(error){
+        res.status(500).json({error: "Något har gått fel med servern"})
+    }
 }); 
 
 //routing för login
 router.post("/login", async (req, res) => {
-    res.json({message: "login called"})
+    try{
+        const { username, password } = req.body;
+
+        if(!username || !password) { //utveckla validering
+            res.status(401).json({message: "Fälten får inte lämnas tomma"}); 
+        }
+
+        if(username === "Stina" && password === "password1"){
+            res.status(200).json({message: "Du har loggats in!"}); 
+        } else {
+            res.status(401).json({error: "fel användarnamn eller lösenord!"}); 
+        }
+
+    } catch(error){
+        res.status(500).json({error: "Något har gått fel med servern"}) 
+    }
 }); 
 
 module.exports = router; 
